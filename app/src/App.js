86777e2231e1aa0015018a41a66a7342a2294202
PauloJP <paulopisani@gmail.com>
2023-06-28 15:51:30 -0300

    Changing visual and using ETHER

--- app/src/App.js
+++ app/src/App.js
@@ -29,9 +29,9 @@ function App() {
   async function newContract() {
     const beneficiary = document.getElementById('beneficiary').value;
     const arbiter = document.getElementById('arbiter').value;
-    const value = ethers.BigNumber.from(document.getElementById('wei').value);
+    const etherValue = document.getElementById('ether').value;
+    const value = ethers.utils.parseEther(etherValue);
     const escrowContract = await deploy(signer, arbiter, beneficiary, value);
-
 
     const escrow = {
       address: escrowContract.address,
@@ -68,8 +68,8 @@ function App() {
         </label>
 
         <label>
-          Deposit Amount (in Wei)
-          <input type="text" id="wei" />
+          Deposit Amount (in Ether)
+          <input type="text" id="ether" />
         </label>
 
         <div
